0ee08e1f05709af1f1ba75e51d715da79c24b5e9
Cbrito03 <63610450+Cbrito03@users.noreply.github.com>
2020-05-27 13:11:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,13 @@ app.listen(port, app, async () => { // Revisa si el mismo esta vivo
 
     console.log('Conectando a MongoDb...');
     //console.log('URL: mongodb://localhost/estadisticaMenuBot');
-    console.log('URL: mongodb://dbaas30.hyperp-dbaas.cloud.ibm.com:28255,dbaas29.hyperp-dbaas.cloud.ibm.com:28214,dbaas31.hyperp-dbaas.cloud.ibm.com:28036/admin?replicaSet=estadisticaMenuBot');
+    //console.log('URL: mongodb://dbaas30.hyperp-dbaas.cloud.ibm.com:28255,dbaas29.hyperp-dbaas.cloud.ibm.com:28214,dbaas31.hyperp-dbaas.cloud.ibm.com:28036/admin?replicaSet=estadisticaMenuBot');
+    console.log('URL: mongodb://'.concat(process.env.URL_MONGODB).concat('/testing_abastible'))
 
-    /*await mongoose.connect('mongodb://dbaas30.hyperp-dbaas.cloud.ibm.com:28255,dbaas29.hyperp-dbaas.cloud.ibm.com:28214,dbaas31.hyperp-dbaas.cloud.ibm.com:28036/admin?replicaSet=estadisticaMenuBot',
+    await mongoose.connect('mongodb://$USERNAME:$PASSWORD@617dc8cb-c565-4f72-b956-09be96d3233b-0.blijs0dd0dcr4f55oehg.databases.appdomain.cloud:32112,617dc8cb-c565-4f72-b956-09be96d3233b-1.blijs0dd0dcr4f55oehg.databases.appdomain.cloud:32112/ibmclouddb?authSource=admin&replicaSet=replset',
     {
-        user: 'sixbell_menuBot',
-        pass: 'S1xb3ll_MenuBot_2o2o',
+        user: 'ibm_cloud_0704e43b_5a90_46e8_a3f6_51248478ada4',
+        pass: '"b2ba3198f486abd8115e2b404282ff52749c090a1a8b7c4135d3f6e85581f78e',
         useNewUrlParser: true,
         useFindAndModify: false,
         useUnifiedTopology: true,
@@ -40,5 +41,5 @@ app.listen(port, app, async () => { // Revisa si el mismo esta vivo
         console.log('No se ha logrado conectar a MongoDb :\n', error)
         console.log('Deteniendo API...')
         process.exit(1)
-    });*/
+    });
 });
